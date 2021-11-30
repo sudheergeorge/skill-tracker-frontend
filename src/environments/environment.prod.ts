35e58af23d26fd1a0ab4b1cad0594c29a9e8c16f
sudheergeorge <38582260+sudheergeorge@users.noreply.github.com>
2021-11-30 21:52:21 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,  
-  searchUrl: 'http://ec2-3-144-224-182.us-east-2.compute.amazonaws.com/api/admin/search'
+  searchUrl: 'https://9p38dkgt03.execute-api.us-east-2.amazonaws.com/v1/search'
 };
